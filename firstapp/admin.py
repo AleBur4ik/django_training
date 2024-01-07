97c37819390b77ca8accbcc59a2e7a96b4fd9d47
Alexey-Vivobook <bur4ik85@mail.ru>
2024-01-08 01:31:47 +0300

    edit firstapp/, add requirements.txt

--- firstapp/admin.py
+++ firstapp/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
 
-# Register your models here.
+admin.site.site_header = "Мой проект"
+admin.site.site_title = "Мой первый проект"
+admin.site.index_title = "Добро пожаловать в Мой проект"
